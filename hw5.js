92bfaa60c9f28c0089277f1d6bef710458d01b1f
Tyler Mathews <mrmathews08@gmail.com>
2020-07-26 14:51:22 -0600

    js work

--- hw5.js
+++ hw5.js
@@ -5,6 +5,7 @@
 $(document).ready(function () {
 
     // First I need to set up my local storage so the input will be saved when I revisit the site
+    // Also set globally to access from anywhere
 
     var keys = object.keys(localStorage);
     for (var i = 0; i < keys.length; i++) {
@@ -12,4 +13,31 @@ $(document).ready(function () {
         var temp = $("#" + keys[i]).find("textarea")
         temp.val(value);
     }
-})
+
+    // Creat a click even for my save button
+    // 
+    // 
+
+
+
+
+    // Insert my correct time and date using moment.js
+
+    $("#currentday").text(moment().format("LLL"));
+    function timeFrame() {
+        var currentHours = moment().hours();
+
+        //target each div with a time-block class,
+        //function to change css information using input from time and day, ie my variables that link moment js
+
+        $(".time-block").each(function() {
+            var hourEl = $(this).attr("id");
+            
+        })
+
+
+
+    }
+
+    timeFrame();
+});
