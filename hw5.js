a9ad088d950f5bd2613a67d72e57c25c04863ea5
Tyler Mathews <mrmathews08@gmail.com>
2020-07-26 15:16:53 -0600

    link js

--- hw5.js
+++ hw5.js
@@ -7,15 +7,22 @@ $(document).ready(function () {
     // First I need to set up my local storage so the input will be saved when I revisit the site
     // Also set globally to access from anywhere
 
-    var keys = object.keys(localStorage);
+    var keys = Object.keys(localStorage);
     for (var i = 0; i < keys.length; i++) {
         var value = localStorage.getItem(keys[i]);
         var temp = $("#" + keys[i]).find("textarea")
         temp.val(value);
     }
 
-    // Creat a click even for my save button
-    // 
+    // Creat a click event for my save button
+    $(".saveBtn").on("click", function(event) {
+        event.preventDefault();
+        console.log(this);
+        var value = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        localStorage.setItem(time, value);
+    });
     // 
 
 
@@ -32,12 +39,38 @@ $(document).ready(function () {
 
         $(".time-block").each(function() {
             var hourEl = $(this).attr("id");
+            //hourDay is a substring of the between 5 and the length of hourEl
+            var hourDay = hourEl.substring(5, hourEl.length);
+            //parse string argument to return an integer
+            var intHourDay = parseInt(hourDay);
+            var intCurrentHours = parseInt(currentHours);
+
+            //doing some math to say: 
+
+            //if intHourDay is less than the current time, add ".past" class and remove the other two (present and future)
+            if (parseInt(intHourDay) < parseInt(intCurrentHours)) {
+                $(this).addClass("past");
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+            }
+            else if (parseInt(intHourDay) > parseInt(intCurrentHours)) {
+                $(this).addClass("future");
+                $(this).removeClass("past");
+                $(this).removeClass("present");
+            }
+            else if (parseInt(intHourDay) === parseInt(intCurrentHours)) {
+                $(this).addClass("present");
+                $(this).removeClass("future");
+                $(this).removeClass("past");
+            }
+
+
             
         })
 
 
 
-    }
+    };
 
     timeFrame();
 });
